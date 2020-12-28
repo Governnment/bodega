0fbb295988afb713add820c61d568414a6b6d5b8
Governnment <governnment@gmail.com>
2020-12-28 15:28:11 +0300

    Nav bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='navbar'>
       <span class='burger'></span>
-      <h1>bodega</h1>
+      <h1>Bodega</h1>
       <div className='search-box'>
         <i className='i-search'></i>
         <p>Search ...</p>
