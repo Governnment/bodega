65b5a70276bc43274e0c6b3346893d6eca56922c
Governnment <governnment@gmail.com>
2021-01-01 22:10:32 +0300

    Delivery time component

---
+++ frontend/src/components/DeliveryTime.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DeliveryTime = () => {
+  return (
+    <div className='deliveryTime'>
+      <i className='i-time' />
+      <p>
+        delivery: <span>now</span>
+      </p>
+    </div>
+  )
+}
+
+export default DeliveryTime
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import CategorySlider from '../components/CategorySlider'
+import DeliveryTime from '../components/DeliveryTime'
 import Recomendations from '../components/Recomendations'
 
 const HomeScreen = () => {
@@ -10,6 +11,7 @@ const HomeScreen = () => {
       <Header />
       <Banner />
       <CategorySlider />
+      <DeliveryTime />
       <Recomendations />
     </>
   )
