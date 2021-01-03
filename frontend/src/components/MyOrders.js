baef268a3d5e89cdaabff46030e41b1709abdf6d
Governnment <governnment@gmail.com>
2021-01-03 22:07:19 +0300

    My orders items

--- frontend/src/components/MyOrders.js
+++ frontend/src/components/MyOrders.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import orderBurger from '../images/order/order-item-burger.png'
+import orderFrenchFries from '../images/order/order-item-french-fries.png'
+import orderSause from '../images/order/order-item-sause.png'
+import delivery from '../images/order/transport.png'
 
 const MyOrders = () => {
   return (
@@ -24,6 +28,33 @@ const MyOrders = () => {
         <Link to='/' className='order-details-choose-time'>
           Choose time
         </Link>
+      </div>
+      <div className='order-items-list'>
+        <div className='order-item'>
+          <img src={orderBurger} alt='burger' />
+          <span>1 x</span>
+          <h3>Beach BBQBurger</h3>
+          <p>$14.99</p>
+        </div>
+        <div className='order-item'>
+          <img src={orderFrenchFries} alt='burger' />
+          <span>1 x</span>
+          <h3>French Fries</h3>
+          <p>$9.99</p>
+        </div>
+        <div className='order-item'>
+          <img src={orderSause} alt='burger' />
+          <span>1 x</span>
+          <h3>Cheese Sause</h3>
+          <p>$0.99</p>
+        </div>
+        <div className='order-delivery'>
+          <div className='wrapper'>
+            <img src={delivery} alt='burger' />
+          </div>
+          <h3>Delivery</h3>
+          <p>$0.00</p>
+        </div>
       </div>
     </div>
   )
