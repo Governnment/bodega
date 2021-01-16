8a582bc2b0e3f4ca5653c96162eeaff5866f637d
Governnment <governnment@gmail.com>
2021-01-16 16:40:52 +0300

    My orders complite

--- frontend/src/components/MyOrders.js
+++ frontend/src/components/MyOrders.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { InputNumber } from 'antd'
+import { TimePicker } from 'antd'
+import moment from 'moment'
 import orderBurger from '../images/order/order-item-burger.png'
 import orderFrenchFries from '../images/order/order-item-french-fries.png'
 import orderSause from '../images/order/order-item-sause.png'
 import delivery from '../images/order/transport.png'
 
 const MyOrders = () => {
+  function onChange(value) {
+    console.log('changed', value)
+  }
+
+  function onChange(time, timeString) {
+    console.log(time, timeString)
+  }
+
   return (
     <div className='my-orders'>
       <i className='i-profile' />
@@ -25,9 +36,13 @@ const MyOrders = () => {
           <i className='i-time' />
         </div>
         <p className='order-details-time'>35 min</p>
-        <Link to='/' className='order-details-choose-time'>
-          Choose time
-        </Link>
+
+        <TimePicker
+          className='order-details-choose-time'
+          placeholder='Choose time'
+          onChange={onChange}
+          defaultOpenValue={moment('00:00:00', 'HH:mm:ss')}
+        />
       </div>
       <div className='order-items-list'>
         <div className='order-item'>
@@ -54,6 +69,24 @@ const MyOrders = () => {
           </div>
           <h3>Delivery</h3>
           <p>$0.00</p>
+        </div>
+        <div className='order-total'>
+          <h4>Total:</h4>
+          <strong>$2325.97</strong>
+          <hr />
+        </div>
+        <div className='order-checkout'>
+          <p>Persons</p>
+          <InputNumber
+            min={1}
+            max={10}
+            defaultValue={3}
+            onChange={onChange}
+            readonly='readonly'
+          />
+          <button>
+            Checkout <i className='i-r-arrow'></i>
+          </button>
         </div>
       </div>
     </div>
